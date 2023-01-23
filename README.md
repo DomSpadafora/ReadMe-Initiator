ab2ef04a31ed662fcc018693094549f5e1ae7a16
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-01-23 17:00:40 -0500

    changes to Readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# ReadMe-Initiator
+# lasjkdf
+
+  ![MIT](https://img.shields.io/badge/license-MIT-green)
+
+  ## Description
+  undefined
+
+  ## Table of Contents
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [License](#license)
+  4. [Contributing](#contributing)
+  5. [Tests](#tests)
+  6. [Questions](#questions)
+
+  ## Installation
+  To install the necessary dependencies, run the following command:
+  undefined
+
+  ## Usage
+  asdlkjasdf
+
+  ## License 
+  This project is licensed under the MIT license.
+
+  ## Contributing
+  undefined
+
+  ## Tests
+  To run tests, run the following command:
+  asdlfjaskd
+  
+  ## Questions
+  If you have any questions about this repo, open an issue or contact me directly at [daf](mailto:daf).
+  You can find more of my work at [asdf](https://www.github.com/asdf).
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   console.log(data)
   return `# ${data.title}
@@ -48,11 +36,6 @@ function generateMarkdown(data) {
   ## Questions
   If you have any questions about this repo, open an issue or contact me directly at [${data.email}](mailto:${data.email}).
   You can find more of my work at [${data.username}](https://www.github.com/${data.username}).
-
-
-
-
-
 
 `;
 }
